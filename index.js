460cbd963a37828d58c4a72cbf53f91b6e3eddc3
Steven Holdaway <webpurellc@gmail.com>
2019-01-22 15:43:13 -0500

    fix: HTML is now formatted

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ module.exports = {
         tag1:{
             process: function (block) {
                 // Format the HTML
-                var formatted_html = this.renderBlock('markdown', block.body)
+                var formatted_html = book.renderBlock('markdown', block.body)
+                    .then(function(data){
+                        return data
+                    });
 
                 var html = "<details>";
                 html += "<summary>Test 1</summary>";
