f53aa5dae5a233d64d15d88b126b3bf544e2d138
Steven Holdaway <webpurellc@gmail.com>
2019-01-22 15:37:56 -0500

    feat: HTML is formatted in output

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ module.exports = {
     blocks: {
         tag1:{
             process: function (block) {
+                // Format the HTML
+                var formatted_html = this.renderBlock('markdown', block.body)
 
                 var html = "<details>";
                 html += "<summary>Test 1</summary>";
-                html += block.body;
+                html += formatted_html;
                 html += "</details>";
 
                 return html;
