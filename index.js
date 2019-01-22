bbca4f780aa0cc0f3ea0f5fcbb9cf2e1f12d0269
Steven Holdaway <webpurellc@gmail.com>
2019-01-22 14:12:42 -0500

    test: first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+    blocks: {
+        collapse: {
+            process: function (blk) {
+                const open = this.output.name !== 'website'
+                const processMarkDown = blk.kwargs.process !== false
+                const title = blk.kwargs.title || ''
+
+                function format(body, summary, shouldOpen) {
+                    const openTag = shouldOpen === true ? 'open': ''
+                    summary = summary || ''
+                    body = body || ''
+
+                    return '<details ' + openTag + '><summary>' + summary + '</summary>' + body + '</details>';
+                }
+
+                return processMarkDown === false ?
+                    format(blk.body, title) :
+                    this.renderBlock('markdown', blk.body, open)
+                        .then(function (data) {
+                            return format(data, title, open)
+                        });
+            }
+        }
+    }
+};
